0ca54b319fdbc4266a8c435a299a67ebcec56cbe
unknown <arturcf00@gmail.com>
2023-03-24 15:15:01 -0300

    second commit

---
+++ student-survey/src/MobilitySurvey.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MobilitySurvey extends Component{
+    render(){
+        return(
+            <div>
+                I am mob survey component
+            </div>
+        )
+    }
+}
+
+export default MobilitySurvey

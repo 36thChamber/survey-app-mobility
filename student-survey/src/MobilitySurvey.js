366505e1b846af5fd059d2e2d42e66634b243ba7
unknown <arturcf00@gmail.com>
2023-03-28 12:31:51 -0300

    fourth commit firewbase 2

--- student-survey/src/MobilitySurvey.js
+++ student-survey/src/MobilitySurvey.js
@@ -1,24 +1,26 @@
 import React, {Component} from 'react';
-var firebase = require('firebase');
-var uuid = require('uuid');
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyDuPnAw9MBb_SAas1Fvb1GaW4FUDVn2eU8",
-    authDomain: "plano-mob.firebaseapp.com",
-    databaseURL: "https://plano-mob-default-rtdb.firebaseio.com",
-    projectId: "plano-mob",
-    storageBucket: "plano-mob.appspot.com",
-    messagingSenderId: "912974969131",
-    appId: "1:912974969131:web:3726b65964663d96732d6e",
-    measurementId: "G-F1WH515TEN"
-  };
+var uuid = require('uuid')
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+const firebaseConfig = {
+  apiKey: "AIzaSyDuPnAw9MBb_SAas1Fvb1GaW4FUDVn2eU8",
+  authDomain: "plano-mob.firebaseapp.com",
+  databaseURL: "https://plano-mob-default-rtdb.firebaseio.com",
+  projectId: "plano-mob",
+  storageBucket: "plano-mob.appspot.com",
+  messagingSenderId: "912974969131",
+  appId: "1:912974969131:web:3726b65964663d96732d6e",
+  measurementId: "G-F1WH515TEN"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 class MobilitySurvey extends Component{
-    constructor(){
+    constructor(props){
         super(props);
 
         this.state={
@@ -31,25 +33,50 @@ class MobilitySurvey extends Component{
                 ans4:'',
                 ans5:'',
             },
-            isSubmitted:false
+            isSubmitted: false
         }
     }
 
     render(){
-        var name = '';
+        var name = '';  
         var questions = '';
         
         if (this.state.studentName == '' && this.state.isSubmitted == false){
             name = <div>
-                <h1>Hey! Please enter your name.</h1>
-                <form>
-                    <input className="sName" type="text" placeholder="Please enter your name" ref="name" />
+                <h1>Por favor coloque seu nome</h1>
+                <form onSubmit={this.studentNameSubmit}>
+                    <input className="sName" type="text" placeholder="Seu Nome" ref="name" />
+                </form>
+            </div>;
+        }
+        else if (this.state.studentName !== ''&& this.state.isSubmitted == false) {
+            name = <div>
+                <h1>Bem vindo {this.state.studentName}!</h1>
+            </div>;
+
+            questions = <div>
+                <h2>Questões</h2>
+                <form onSubmit="">
+                    <div className='card'>
+                        <label>?????</label>
+                        <input type="111" name="222" value="333">
+
+                        </input>
+                    </div>
                 </form>
             </div>
+    
+        
         }
+
         return(
             <div>
-                I am mob survey component
+               {name}
+
+               =======================================
+
+               {questions}
+
             </div>
         )
     }
